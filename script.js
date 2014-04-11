aaed0e0422ce77e9b0ccfdb6894e5018b420bc1b
Simon Rai <simon@raize.ca>
2014-04-11 02:11:59 +0000

    added instagram api

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-$(function() {
-    function loadPhoto() {
-    var url = 'https://api.instagram.com/v1/tags/snow/media/recent?access_token=960620.f59def8.1e7aae81446047ee89d984d1045455de';
-    $.ajax(url, {
-        dataType: 'jsonp',
-        success: function(data) {
-            var photo = data.first();
-            $('<img src=\"' + photo.data.images.standard_url + '\"/>').appendTo($('body'));
-        }
-    };
-});
+function grabImages(access_parameters) {
+
+	var instagramUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count='+ count; 
+	
+	$.getJSON(instagramUrl, access_parameters, onDataLoaded);
+
+}
+
+function onDataLoaded(instagram_data) {
+
+  if(instagram_data.meta.code == 200) {
+
+    var photos = instagram_data.data;
+
+    if(photos.length > 0) {
+      
+      for (var key in photos ){
+
+        var photo = photos[key];
+
+        $('#target').append('');
+      }
+    }
+    else {
+
+      $('#target').append("Hmm.  I couldn’t find anything!");
+    	} else  {
+
+      var error = data.meta.error_message;
+      
+      $('#target').append('Something happened, Instagram said: ’ + error');
+    }
+  }
+}  
